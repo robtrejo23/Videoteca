45e6a4950d42b2492e07857c64cfb66b6f337e77
Roberto Carlos Trejo Jiménez <101604746+robtrejo23@users.noreply.github.com>
2022-04-25 17:54:56 -0500

    modified:   src/app/app.module.ts 	new file:   src/app/pipes/imagen.pipe.ts 	new file:   src/app/pipes/pipes.module.ts 	modified:   src/app/tab1/tab1.module.ts 	modified:   src/app/tab1/tab1.page.html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { AppComponent } from './app.component';
 
 import{ HttpClient, HttpClientModule} from '@angular/common/http';
 
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
---
+++ src/app/pipes/imagen.pipe.ts
@@ -0,0 +1,21 @@
+import { Pipe, PipeTransform } from '@angular/core';
+ 
+const URL = 'https://image.tmdb.org/t/p';
+
+@Pipe({
+  name: 'imagen'
+})
+export class ImagenPipe implements PipeTransform {
+
+  transform(img: string, size:string = 'w500'): string {
+   
+   if(!img){
+   return;
+   }
+   const urlImg = `${ URL }/${ size }${ img }`; 
+
+   
+    return urlImg;
+  }
+
+}
